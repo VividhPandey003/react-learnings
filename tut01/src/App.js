34080707ca7be3c62af59bee91dbf705a3cdf9f2
Vividh Pandey <vividh.pandey.13@gmail.com>
2023-09-12 00:53:12 +0530

    Added Files

--- tut01/src/App.js
+++ tut01/src/App.js
@@ -1,13 +1,63 @@
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
+import AddItem from './AddItem';
+import { useState } from 'react';
 
 function App() {
+
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "Item 1"
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3"
+    }
+  ]);
+
+  const [newItem, setNewItem] = useState('')
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  }
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    console.log("submitted");
+  }
+
   return (
     <div className="App">
-      <Header />
-      <Content />
-      <Footer/>
+      <Header title="Grocery List" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
