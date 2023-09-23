c550c925f844b48fa7dff871243aa06ac78399f6
Vividh Pandey <vividh.pandey.13@gmail.com>
2023-09-23 21:05:45 +0530

    Updated Files

--- crudpractice/src/App.js
+++ crudpractice/src/App.js
@@ -1,7 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
 
 function App() {
+  const API_URL = 'https://jsonplaceholder.typicode.com/';
+  const [reqType, setReqType] = useState('users');
+  const [items,setItems] = useState([]);
+
+  useEffect(()=>{
+
+    const fetchItems = async()=>{
+      try{
+        const response = await fetch('${API_URL}')
+      }
+      catch{
+
+      }
+    }
+  },[reqType])
   return (
     <div className="App">
       
