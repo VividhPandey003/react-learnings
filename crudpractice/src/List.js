4fe697764c2ac4538a6eeaf3a163890eaa1b165c
Vividh Pandey <vividh.pandey.13@gmail.com>
2023-09-24 00:54:42 +0530

    Updated List

--- crudpractice/src/List.js
+++ crudpractice/src/List.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import ListItem from './ListItem'
 
-const List = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const List = ({ items }) => {
+    return (
+        <ul>
+            {items.map(item => (
+                <ListItem key={item.id} item={item} />
+            ))}
+        </ul>
+    )
 }
 
 export default List
