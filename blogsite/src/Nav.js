3538c9689f4fc4125a1394e270afe48f91a98a37
Vividh Pandey <vividh.pandey.13@gmail.com>
2023-09-25 00:07:09 +0530

    Updated Files

--- blogsite/src/Nav.js
+++ blogsite/src/Nav.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const Nav = () => {
+const Nav = ({search,setSearch}) => {
   return (
-    <nav>
-      <h1>Nav</h1>
+    <nav className='Nav'>
+      <form className='searchForm' onSubmit={(e)=> e.preventDefault}>
+        <label htmlFor='search'>Search Posts</label>
+        <input
+          id='search'
+          type='text'
+          placeholder='Search Posts'
+          value={search}
+          onChange={(e) =>setSearch(e.target.value)}
+        />
+      </form>
+      <ul>
+        <li>
+          
+        </li>
+      </ul>
     </nav>
   )
 }
